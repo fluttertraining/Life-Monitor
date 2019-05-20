8e80621fbdfb9768ad0311074a5e29b6f1dee2a7
Sean Urgel <seantheurgel@gmail.com>
2019-05-20 20:36:36 +0800

    center the congratulations

--- lib/screens/add_bank_success.dart
+++ lib/screens/add_bank_success.dart
@@ -6,53 +6,50 @@ class AddBankSuccessScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          child: ListView(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  SizedBox(height: 50),
-                  Image.asset(
-                    'assets/images/confetti.png',
-                    height: 175,
-                    width: MediaQuery.of(context).size.width,
-                    filterQuality: FilterQuality.high,
-                  ),
-                  SizedBox(height: 40),
-                  Text(
-                    'Congratulations!',
-                    style: Theme.of(context).textTheme.title,
-                  ),
-                  SizedBox(height: 20),
-                  Container(
-                    width: MediaQuery.of(context).size.width * .8,
-                    child: Text(
-                      'Your bank account is added successfully to the app!',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w300,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  SizedBox(height: 40),
-                  CustomButton(
-                    child: Text(
-                      'Get Started',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                      ),
-                    ),
-                    width: MediaQuery.of(context).size.width * .8,
-                    height: 60,
-                    onTap: () {},
-                  )
-                ],
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/confetti.png',
+              height: 175,
+              width: MediaQuery.of(context).size.width,
+              filterQuality: FilterQuality.high,
+            ),
+            SizedBox(height: 40),
+            Text(
+              'Congratulations!',
+              style: Theme.of(context).textTheme.title,
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width * .8,
+              child: Text(
+                'Your bank account is added successfully to the app!',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300,
+                ),
+                textAlign: TextAlign.center,
               ),
-            ],
-          ),
+            ),
+            SizedBox(height: 40),
+            CustomButton(
+              child: Text(
+                'Get Started',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              width: MediaQuery.of(context).size.width * .8,
+              height: 60,
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
         ),
       ),
     );
